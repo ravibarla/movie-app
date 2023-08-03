3430151cb8f24334cedf3fdb53139d38b52bd074
ravi barla <ravibarla1@gmail.com>
2023-08-03 16:09:02 +0530

    adding movieCard and Navbar

---
+++ src/component/App.js
@@ -0,0 +1,24 @@
+import "../App.css";
+import { data } from "../data";
+import MovieCard from "./MovieCard";
+import Navbar from "./Navbar";
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="main">
+        <div className="tabs">
+          <div className="tab">movies</div>
+          <div className="tab">favourites</div>
+        </div>
+        <div className="list">
+          {data.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
