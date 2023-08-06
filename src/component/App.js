9d20be9d94dbece3e7b3466b2762651f3c6b8595
ravi barla <ravibarla1@gmail.com>
2023-08-06 10:41:53 +0530

    adding store

--- src/component/App.js
+++ src/component/App.js
@@ -2,7 +2,12 @@ import "../App.css";
 import { data } from "../data";
 import MovieCard from "./MovieCard";
 import Navbar from "./Navbar";
+import { createStore } from "redux";
+import movies from "../reducers";
 function App() {
+  const store = createStore(movies);
+  console.log("store : ",store)
+  console.log("state :",store.getState())
   return (
     <div className="App">
       <Navbar />
