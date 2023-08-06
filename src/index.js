fd897f5d1f6b75c172734da3c98d9662313c6cfc
ravi barla <ravibarla1@gmail.com>
2023-08-06 10:50:41 +0530

    adding dispatch

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./component/App";
 import { createStore } from "redux";
-import movies from "../reducers";
+import movies from "./reducers";
 
 const store = createStore(movies);
-console.log("store : ",store)
-console.log("state :",store.getState())
+console.log("store : ", store);
+console.log("before state :", store.getState());
+store.dispatch({
+  type: "ADD_MOVIES",
+  movies: [{ name: "superman" }],
+});
+console.log("after state :", store.getState());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
