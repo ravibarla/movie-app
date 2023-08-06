d378c30d7e3d5a579b01165ac3bf5205abff02da
ravi barla <ravibarla1@gmail.com>
2023-08-06 11:20:42 +0530

    adding dispatch and understanding subscribe

--- src/component/App.js
+++ src/component/App.js
@@ -1,27 +1,48 @@
+import React from "react";
 import "../App.css";
 import { data } from "../data";
 import MovieCard from "./MovieCard";
 import Navbar from "./Navbar";
-import { createStore } from "redux";
-import movies from "../reducers";
-function App() {
- 
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="main">
-        <div className="tabs">
-          <div className="tab">movies</div>
-          <div className="tab">favourites</div>
-        </div>
-        <div className="list">
-          {data.map((movie,index) => (
-            <MovieCard movie={movie} key={`movies-${index}`}/>
-          ))}
+
+// import movies from "../reducers";
+class App extends React.Component {
+  componentDidMount() {
+    const { store } = this.props;
+    store.subscribe(() => {
+      // console.log("updating :");
+    });
+    //make api call
+    //dispatch action
+    store.dispatch({
+      type: "ADD_",
+      movies: data,
+    });
+    // console.log("state :", store.getState());
+  }
+
+  render() {
+    // console.log("this props :", this.props);
+    // const { store } = this.props;
+    // console.log("getState :",store.getState())
+    // const movies = store.getState();
+    // console.log("movies :", movies);
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="main">
+          <div className="tabs">
+            <div className="tab">movies</div>
+            <div className="tab">favourites</div>
+          </div>
+          <div className="list">
+            {data.map((movie, index) => (
+              <MovieCard movie={movie} key={`movies-${index}`} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
