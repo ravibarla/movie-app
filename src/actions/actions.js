4daf718e61e6419fa4ee963b807e6671fc1a9927
ravi barla <ravibarla1@gmail.com>
2023-08-06 11:30:23 +0530

    adding action creator

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -7,3 +7,10 @@
 
 //action type
 export const ADD_MOVIES = "add movies";
+//action creator
+export const addMovie = (movies) => {
+    return {
+      type: ADD_MOVIES,
+      movies,
+    };
+  };
