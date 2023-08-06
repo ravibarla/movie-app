af3cbf80451c812fd33806fb271920950d1cd334
ravi barla <ravibarla1@gmail.com>
2023-08-06 11:32:37 +0530

    adding reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { ADD_MOVIES } from "../actions/actions";
+export default function movies(state = [], action) {
+  if (action.type === ADD_MOVIES) {
+    return action.movies;
+  }
+  return state;
+}
+
